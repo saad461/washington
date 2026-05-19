86852a7cd06dd459857cc075383236bb86e27497
saad461 <76460749+saad461@users.noreply.github.com>
2026-05-19 18:49:58 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,10 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: [
-        '/api/', 
-        '/tmp/'
-      ],
     },
     sitemap: 'https://wscss.site/sitemap.xml',
   };
